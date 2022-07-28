b8e9b5618a5b17b21145288554656e62dbb585de
Jurkenas <mjurkenas@gmail.com>
2022-07-28 21:18:17 +0300

    Interactive tank game initial commit

--- TankTask/src/Main.java
+++ TankTask/src/Main.java
@@ -1,24 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Tankas tankas1 = new Tankas(0, 0, Kryptis.RYTAI);
-        tankas1.kairen();
-        tankas1.kairen();
-        tankas1.suvis();
-        tankas1.desinen();
-        tankas1.atgal();
-        tankas1.pirmyn();
-        tankas1.suvis();
-        tankas1.kairen();
-        tankas1.kairen();
-        tankas1.info();
-        tankas1.suvis();
-        tankas1.kairen();
-        tankas1.kairen();
-        tankas1.suvis();
-        tankas1.suvis();
-        tankas1.atgal();
-        tankas1.suvis();
-        tankas1.suvis();
-        tankas1.info();
+        Scanner scanner = new Scanner(System.in);
+        boolean runProgram = true;
+        while(runProgram){
+            showCommands();
+            runProgram = false;
+        }
+        scanner.close();
+    }
+
+    public static void showCommands() {
+        System.out.println("----------- PASIRINKITE -----------");
+        System.out.printf("%5s - %-15s", "[s]", "ėjimas į Šiaurę");
+        System.out.printf("\n%5s - %-15s", "[r]", "ėjimas į Rytus");
+        System.out.printf("\n%5s - %-15s", "[p]", "ėjimas į Pietus");
+        System.out.printf("\n%5s - %-15s", "[v]", "ėjimas į Vakarus");
+        System.out.printf("\n%5s - %-15s", "[*]", "šūvis");
+        System.out.printf("\n%5s - %-15s", "[i]", "info");
+        System.out.printf("\n%5s - %-15s", "[x]", "pabaiga");
+        System.out.println("\n-----------------------------------");
     }
 }
